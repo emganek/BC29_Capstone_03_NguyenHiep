98730868371f089715b4cf3c59e4344a71a26236
emganek <nickphuthoi12399@gmail.com>
2022-08-08 18:28:48 +0700

    home page, login page, movie detail page

---
+++ src/pages/movieDetail/movieDetail.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import MovieDetail from '../../modules/movieDetail/movieDetail'
+import ShowTime from '../../modules/showTime/showTime'
+export default function MovieDetailPage() {
+    return (
+        <div className="py-5 mt-md-5">
+            <div className="container">
+                <div className="row">
+                    <div id="movie-detail" className="col-12">
+                        <MovieDetail />
+                    </div>
+                    <div id="showtime" className="col-12 mt-4">
+                        <ShowTime />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
